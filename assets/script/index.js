303e5b7702a4f2ae0446247033f8f85990cffc87
Ilvina Rakhimova <ilvrakh@gmail.com>
2022-08-23 21:49:45 +0500

    mistakes correction

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,15 +1,8 @@
 "use strict";
 
-document.addEventListener('DOMContentLoaded', () => {
-    const inputElement = document.getElementById('birthYear') // ищем наш единственный input
-    const maskOptions = { // создаем объект параметров
-        mask: '0000' // задаем единственный параметр mask
-    }
-    IMask(inputElement, maskOptions) // запускаем плагин с переданными параметрами
-})
-
 const button = document.getElementById('register');
 
+const login = document.getElementById('login');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const reEnterPassword = document.getElementById('reEnterPassword');
@@ -18,136 +11,107 @@ const birthYear = document.getElementById('birthYear');
 const errorMessage =  document.getElementById('errorMessage');
 
 
-button.onmouseover = check;
-
-function check(){
-    // checkBlankEmail();
-
+const check = () => {
+    checkBlankLogin();
     checkValidEMail();
-    // checkBlankPassword();
-    checkBlankReEnterPassword();
-    // checkBlankBirthYear();
-
     checkPasswordLength();
+    checkBlankReEnterPassword();
     checkEqualPassword();
-
     checkValidBirthYear();
     checkAge();
-
     checkCheckbox();
-}
+
+    greeting();
+};
+
+const checkBlankLogin = () => {
+    document.getElementById('loginError').innerHTML = '';
+    login.classList.remove("error__input");
+    if (login.value === '') {
+        document.getElementById('loginError').innerHTML = 'Please enter your login.';
+        login.classList.add("error__input");
+    }
+};
 
 const checkValidEMail = () => {
     const reg = /^[\w-\.]+@[\w-]+\.[a-z]{2,4}$/i;
     const valid = reg.test(email.value);
     document.getElementById('emailError').innerHTML = '';
+    email.classList.remove("error__input");
     if (! valid) { 
         document.getElementById('emailError').innerHTML = 'Please enter a valid email address.';
+        email.classList.add("error__input");
     }
-}
-
-// const checkBlankEmail = () => {
-
-//     document.getElementById('emailError').innerHTML = '';
-//     if (email.value == '') {
-//         document.getElementById('emailError').innerHTML = 'Please enter a valid email address.';
-//     }
-// }
-
-// const checkBlankPassword = () => {
-    
-//     document.getElementById('passwordError').innerHTML = '';
-//     if (password.value == '') {
-//         document.getElementById('passwordError').innerHTML = 'Please enter your password.';
-//     }
-// }
-
-const checkBlankReEnterPassword = () => {
-    
-    document.getElementById('reEnterPasswordError').innerHTML = '';
-    if (reEnterPassword.value == '') {
-        document.getElementById('reEnterPasswordError').innerHTML = 'Please confirm your password. <br/>';
-    }
-}
-
-// const checkBlankBirthYear = () => {
-    
-//     document.getElementById('birthYearError').innerHTML = '';
-//     if (birthYear.value == '') {
-//         document.getElementById('birthYearError').innerHTML = 'Please enter your year of birth.';
-//     }
-// }
-
+};
 
 const checkPasswordLength = () => {
     document.getElementById('passwordError').innerHTML = '';
+    password.classList.remove("error__input");
     if (password.value.length <= 5) {
         document.getElementById('passwordError').innerHTML += 'Your password should be at least 6 characters long.';
+        password.classList.add("error__input");
     }
-}
+};
+
+const checkBlankReEnterPassword = () => {
+    document.getElementById('reEnterPasswordError').innerHTML = '';
+    reEnterPassword.classList.remove("error__input");
+    if (reEnterPassword.value === '') {
+        document.getElementById('reEnterPasswordError').innerHTML = 'Please confirm your password. <br/>';
+        reEnterPassword.classList.add("error__input");
+    }
+};
 
 const checkEqualPassword = () => {
+    reEnterPassword.classList.remove("error__input");
     if (password.value != reEnterPassword.value) {
         document.getElementById('reEnterPasswordError').innerHTML += 'Please enter the same passwords in both password fields.';
+        reEnterPassword.classList.add("error__input");
     }
-}
+};
 
 const checkValidBirthYear = () => {
     const birthYearNumber = birthYear.value;
     document.getElementById('birthYearError').innerHTML = '';
-    if (birthYearNumber < 1900 || birthYearNumber >= 2022 || birthYearNumber == '') {
+    birthYear.classList.remove("error__input");
+    if (birthYearNumber < 1900 || birthYearNumber >= 2022 || birthYearNumber === '') {
         document.getElementById('birthYearError').innerHTML = 'Please enter a valid birth year.';
+        birthYear.classList.add("error__input");
     }
-}
+};
 
 const checkAge = () => {
-    // const birthYearNumber = birthYear.value;
     const age = 2022 - birthYear.value;
 
     errorMessage.innerHTML = '';
-    
+
     if (age < 13) {
         errorMessage.classList.add("error__message_border");
         errorMessage.innerHTML = 'Sorry, you must be at least 13 years old in order to use this website.<br/>';
     }
-}
+};
 
 const checkCheckbox = () => {
     const checkbox = document.getElementById("terms");
-    errorMessage.classList.remove("error__message_border");;
-    if (checkbox.checked != true) {
+    errorMessage.classList.remove("error__message_border");
+    if (checkbox.checked !== true) {
         errorMessage.classList.add("error__message_border");
         errorMessage.innerHTML += 'You must agree to the Steam Subscriber Agreement to continue.';
     }
-}
+};
 
-// const button = document.getElementById('register');
-// button.onmouseover = check;
+const greeting = () => {
+    if ((document.getElementById('loginError').innerHTML === '') && (document.getElementById('emailError').innerHTML === '') && (document.getElementById('passwordError').innerHTML === '') && (document.getElementById('reEnterPasswordError').innerHTML === '') && (document.getElementById('birthYearError').innerHTML === '') && (errorMessage.innerHTML == '')) {
+        alert(`Welcome to the website, ${login.value}!`);
+    }
+};
 
-// function check() {
-//     const email = document.getElementById('email');
-//     const password = document.getElementById('password');
-//     const reEnterPassword = document.getElementById('reEnterPassword');
-//     const birthYear = document.getElementById('birthYear');
+document.addEventListener('DOMContentLoaded', () => {
+    const inputElement = document.getElementById('birthYear') // ищем наш единственный input
+    const maskOptions = { // создаем объект параметров
+        mask: '0000' // задаем единственный параметр mask
+    }
+    IMask(inputElement, maskOptions) // запускаем плагин с переданными параметрами
+});
 
-//     document.getElementById('emailError').innerHTML = '';
-//     document.getElementById('passwordError').innerHTML = '';
-//     document.getElementById('reEnterPasswordError').innerHTML = '';
-//     document.getElementById('birthYearError').innerHTML = '';
-
-//     if (email.value == '') {
-//         document.getElementById('emailError').innerHTML = 'Please enter a valid email address.';
-//     }
-
-//     if (email.value == '') {
-//         document.getElementById('passwordError').innerHTML = 'Please enter your password.';
-//     }
-
-//     if (email.value == '') {
-//         document.getElementById('reEnterPasswordError').innerHTML = 'Please confirm your password.';
-//     }
-
-//     if (email.value == '') {
-//         document.getElementById('birthYearError').innerHTML = 'Please enter your year of birth.';
-//     }
-// }
+button.addEventListener('click', check);
